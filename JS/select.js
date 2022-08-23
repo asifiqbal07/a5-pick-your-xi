d5d7bb338b3402960c1ad028d2806a534e791ca8
asifiqbal07 <as.asif.07@gmail.com>
2022-08-23 18:58:34 +0600

    footer and js added

---
+++ JS/select.js
@@ -0,0 +1,37 @@
+const selectedPlayers = [];
+
+function display(selectedPlayers) {
+    console.log(selectedPlayers);
+
+    const selectedTable = document.getElementById("selected-v");
+    selectedTable.innerHTML = "";
+    for (let i = 0; i < selectedPlayers.length; i++) {
+        // console.log(selectedPlayers[i]);
+
+        const clickedPlayerName = selectedPlayers[i].playerName
+        const tr = document.createElement("tr");
+
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${clickedPlayerName}</td>`;
+
+        selectedTable.appendChild(tr);
+            
+    }
+}
+
+function selectPlayer(element) {
+    // console.log(element.parentNode.parentNode.children)
+    // console.log(element.parentNode.parentNode.children[01].innerText)
+
+    const playerName = element.parentNode.parentNode.children[01].innerText;
+    
+    const playerObj = {
+        playerName: playerName
+    }
+
+    selectedPlayers.push(playerObj);
+
+
+    display(selectedPlayers);
+}

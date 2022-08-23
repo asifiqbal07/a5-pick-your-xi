0bb104ea877bfea13df960c91eff8b0ca506b4a4
asifiqbal07 <as.asif.07@gmail.com>
2022-08-23 20:54:59 +0600

    selected player added

--- JS/select.js
+++ JS/select.js
@@ -5,9 +5,10 @@ function display(selectedPlayers) {
 
     const selectedTable = document.getElementById("selected-v");
     selectedTable.innerHTML = "";
-    for (let i = 0; i < selectedPlayers.length; i++) {
-        // console.log(selectedPlayers[i]);
 
+    for (let i = 0; i < selectedPlayers.length; i++) {
+
+        // console.log(selectedPlayers[i]);}
         const clickedPlayerName = selectedPlayers[i].playerName
         const tr = document.createElement("tr");
 
@@ -16,22 +17,35 @@ function display(selectedPlayers) {
         <td>${clickedPlayerName}</td>`;
 
         selectedTable.appendChild(tr);
-            
+
     }
+
 }
+
 
 function selectPlayer(element) {
     // console.log(element.parentNode.parentNode.children)
     // console.log(element.parentNode.parentNode.children[01].innerText)
 
     const playerName = element.parentNode.parentNode.children[01].innerText;
-    
+
     const playerObj = {
         playerName: playerName
     }
 
+    element.disabled = true;
     selectedPlayers.push(playerObj);
 
 
     display(selectedPlayers);
 }
+
+// const button = document.querySelector('#button');
+
+// const disableButton = () => {
+//   console.log("va");
+//     button.disabled = true;
+
+// };
+
+// button.addEventListener('click', disableButton);
